b82333ccf0d6cee2d494a19e4909eebf9afd8987
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-02-21 20:57:50 -0300

    style: format Button.test file

--- app/resources/common/components/__tests__/Button.test.tsx
+++ app/resources/common/components/__tests__/Button.test.tsx
@@ -1,76 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import Button from '../Button';
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import Button from "../Button";
+import "@testing-library/jest-dom";
 
-describe('Button Component', () => {
-  it('should render with default props', () => {
-    render(
-      <Button>
-        Rótulo
-      </Button>
+describe("Button Component", () => {
+  it("should render with default props", () => {
+    render(<Button>Rótulo</Button>);
+    const button = screen.getByRole("button", { name: /Rótulo/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "py-2 px-4 text-xl h-12 rounded-md bg-primary text-white"
     );
-    const button = screen.getByRole('button', { name: /Rótulo/i });
-    expect(
-      button
-    ).toBeInTheDocument();
-    expect(
-      button
-    ).toHaveClass('py-2 px-4 text-xl h-12 rounded-md bg-primary text-white');
   });
-  it('should render with lighter variant', () => {
-    render(
-      <Button variant='lighter'>
-        Rótulo
-      </Button>
+  it("should render with lighter variant", () => {
+    render(<Button variant="lighter">Rótulo</Button>);
+    const button = screen.getByRole("button", { name: /Rótulo/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "py-2 px-4 text-xl h-12 rounded-md bg-primary-lighter text-primary"
     );
-    const button = screen.getByRole('button', { name: /Rótulo/i });
-    expect(
-      button
-    ).toBeInTheDocument();
-    expect(
-      button
-    ).toHaveClass('py-2 px-4 text-xl h-12 rounded-md bg-primary-lighter text-primary');
   });
-  it('should render with secondary variant', () => {
-    render(
-      <Button variant='secondary'>
-        Rótulo
-      </Button>
+  it("should render with secondary variant", () => {
+    render(<Button variant="secondary">Rótulo</Button>);
+    const button = screen.getByRole("button", { name: /Rótulo/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "py-2 px-4 text-xl h-12 rounded-md bg-secondary text-primary"
     );
-    const button = screen.getByRole('button', { name: /Rótulo/i });
-    expect(
-      button
-    ).toBeInTheDocument();
-    expect(
-      button
-    ).toHaveClass('py-2 px-4 text-xl h-12 rounded-md bg-secondary text-primary');
   });
-  it('should render with small size', () => {
-    render(
-      <Button size='small'>
-        Rótulo
-      </Button>
+  it("should render with small size", () => {
+    render(<Button size="small">Rótulo</Button>);
+    const button = screen.getByRole("button", { name: /Rótulo/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "py-1.5 px-3 text-base h-9 rounded bg-primary text-white"
     );
-    const button = screen.getByRole('button', { name: /Rótulo/i });
-    expect(
-      button
-    ).toBeInTheDocument();
-    expect(
-      button
-    ).toHaveClass('py-1.5 px-3 text-base h-9 rounded bg-primary text-white');
   });
-  it('should render with large size', () => {
-    render(
-      <Button size='large'>
-        Rótulo
-      </Button>
+  it("should render with large size", () => {
+    render(<Button size="large">Rótulo</Button>);
+    const button = screen.getByRole("button", { name: /Rótulo/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass(
+      "py-2.5 px-5 text-2xl h-14 rounded-lg bg-primary text-white"
     );
-    const button = screen.getByRole('button', { name: /Rótulo/i });
-    expect(
-      button
-    ).toBeInTheDocument();
-    expect(
-      button
-    ).toHaveClass('py-2.5 px-5 text-2xl h-14 rounded-lg bg-primary text-white');
   });
 });
