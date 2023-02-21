0e5aa8bc7b500dc9abd8e724ce6ca3f0786ed610
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-02-21 15:15:00 -0300

    fix(lang): change html language

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export const meta: MetaFunction = () => ({
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <Meta />
         <Links />
