8e95adfbf46d224eba10cd2c54d22508bf04eeb0
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-02-21 14:58:42 -0300

    feat: add cc colors on tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#161616",
+        "primary-light": "#676767",
+        "primary-lighter": "#B5B5B5",
+        secondary: "#F5F5F5",
+        "secondary-dark": "#F3F3F3",
+        accent: "#FFED4F",
+        "light-blue": "#B8F6F5",
+      },
+    },
   },
   plugins: [],
 }
