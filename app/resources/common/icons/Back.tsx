d9f5c63bc6b324af159bf64738f69b1dbd8b1e36
William Gonçalves <euwilliamgoncalves@gmail.com>
2023-02-22 14:03:04 -0300

    feat: add Back and Copy icons

---
+++ app/resources/common/icons/Back.tsx
@@ -0,0 +1,18 @@
+export default function Back() {
+  return (
+    <svg
+      width="16"
+      height="14"
+      viewBox="0 0 16 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.29292 0.792969L7.70714 2.20718L3.91424 6.00008H16V8.00008H3.91424L7.70714 11.793L6.29292 13.2072L0.0858154 7.00008L6.29292 0.792969Z"
+        fill="#161616"
+      />
+    </svg>
+  );
+}
---
+++ app/resources/common/icons/Copy.tsx
@@ -0,0 +1,22 @@
+export default function Copy() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16 0H6C4.94023 0 4.07298 0.824263 4.00437 1.86667H16.1333V13.9956C17.1757 13.927 18 13.0598 18 12V2C18 0.895431 17.1046 0 16 0Z"
+        fill="#161616"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 16H12V6H2V16ZM12 18C13.1046 18 14 17.1046 14 16V6C14 4.89543 13.1046 4 12 4H2C0.895431 4 0 4.89543 0 6V16C0 17.1046 0.895431 18 2 18H12Z"
+        fill="#161616"
+      />
+    </svg>
+  );
+}
